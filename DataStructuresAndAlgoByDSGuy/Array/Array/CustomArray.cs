31ed9d964c54c105e52876264a26395401f3be89
Tushar Patil <tushar-patil.ext@siemens.com>
2019-12-25 18:06:18 +0530

    added first project

---
+++ DataStructuresAndAlgoByDSGuy/Array/Array/CustomArray.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+   public class CustomArray<T>
+    {
+        private T[] arr;
+        private int size;
+        public CustomArray(int sizeOfArray,T defaultValue)
+        {
+            arr = new T[sizeOfArray];
+            size = sizeOfArray;
+            if (!defaultValue.Equals(default(T)))
+            {
+
+            }
+        }
+
+        public void Traverse()
+        {
+            try
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine($"element at {i} is {arr[i]} ");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Array no longer exists");
+                
+            }
+        }
+
+        public void InsertAt(int location,T valueToBeInserted)
+        {
+            try
+            {
+                if (arr[location].Equals(default(T)))
+                {
+                    arr[location] = valueToBeInserted;
+                }
+                else
+                {
+                    Console.WriteLine("location is already occupied");
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Invalid index to access array");
+            }
+        }
+
+        public T AccessCell(int location)
+        {
+            T cellValue = default(T);
+            try
+            {
+                cellValue = arr[location];
+                Console.WriteLine($"Value at location {location} is {cellValue}");
+                
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid index to access array");
+            }
+            return cellValue;
+        }
+
+        public int Search(T value)
+        {
+            int location = -1;
+            try
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (arr[i].Equals(value))
+                    {
+                        location = i;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Array no longer exists");
+            }
+            return location;
+        }
+
+        public void Delete(int location)
+        {
+            try
+            {
+                arr[location] = default(T);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid index for array");
+                
+            }
+        }
+    }
+}
---
+++ DataStructuresAndAlgoByDSGuy/Array/Array/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Arrays
+{
+    class Animal
+    {
+    }
+    class Mammoth : Animal
+    {
+
+    }
+    class Giraffe : Animal
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Array<Animal> a = new Array<Animal>();
+            //Mammoth[] mammoths = new Mammoth[10];
+           // Animal[] animals = mammoths;
+            // animals[1] = new Giraffe();
+            
+           // int [] arr;
+           // arr = new int[10] ;
+           // InsertForArray(arr, 2, 1);
+            //InsertForArray(arr, 2, 1);
+            // InsertForArray(arr, 10, 10);
+          //  Console.WriteLine(AccessCell(arr,1));
+          CustomArray<int> customArray = new CustomArray<int>()
+        }
+        static void DeleteItem<T>(T[] arr, int location)
+        {
+            if (location < 0 || location >= arr.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+           else if (arr[location].Equals(default(T)))
+            {
+                throw new Exception("No value is present");
+            }
+            else
+            {
+                arr[location] = default(T);
+            }
+        }
+        static int SearchArray<T>(T[] arr, T value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Equals(value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static T AccessCell<T>(T[] arr, int location)
+        {
+            if (location >= arr.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                return arr[location];
+            }
+        }
+        static void InsertForArray<T>(T[] arr, T value, int location)
+        {
+            if (location >= arr.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (!arr[location].Equals(default(T)))
+            {
+                throw new Exception("Item already Exists");
+            }
+            else
+            {
+                arr[location] = value;
+            }
+        }
+    }
+}
