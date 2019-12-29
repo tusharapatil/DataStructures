d58bb2909fc135a2858a63bd257e9d53d1bc5530
tusharAp <tusharr31.7.90@gmail.com>
2019-12-29 16:19:15 +0530

    Completed array

---
+++ DataStructuresAndAlgoByDSGuy/Array/Array/Custom2DArray.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+   public class Custom2DArray<T>
+    {
+        private T[,] arr;
+        private T defaultValue = default(T);
+        private int numberOfColumns;
+        private int numberOfRows;
+
+        public Custom2DArray(int noOfRows, int noOfCols, T defaultValue = default(T))
+        {
+            Console.WriteLine($"Creating 2D array with Rows {noOfRows} and Columns {noOfCols} with default value {defaultValue}");
+            arr = new T[noOfRows,noOfCols];
+            numberOfColumns = noOfCols;
+            numberOfRows = noOfRows;
+            if (!defaultValue.Equals(default(T)))
+            {
+                this.defaultValue = defaultValue;
+                for (int i = 0; i < noOfRows; i++)
+                {
+                    for (int j = 0; j < noOfCols; j++)
+                    {
+                        arr[i,j] = defaultValue;
+                    }
+                }
+            }
+        }
+                        
+        public void Traverse()
+        {
+            try
+            {
+                for (int i = 0; i < numberOfRows; i++)
+                {
+                    for (int j = 0;  j < numberOfColumns;  j++)
+                    {
+                        Console.Write(arr[i,j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Array does not exists");
+            }
+        }
+
+        public void Insert(int rowNumber, int colNumber, T value)
+        {
+            try
+            {
+                if (arr == null)
+                {
+                    throw new Exception("Array does not exist!!");
+                }
+                if (arr[rowNumber, colNumber].Equals(defaultValue))
+                {
+                    Console.WriteLine($"Setting cell rowNumber {rowNumber} colNumber {colNumber} to value {value}");
+                    arr[rowNumber, colNumber] = value;
+                }
+                else
+                {
+                    throw new Exception($"value for cell rowNum {rowNumber} colNumber {colNumber} already exists");
+
+
+                }
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+
+                Console.WriteLine($"Invalid Index used for {( rowNumber >= this.numberOfRows ? "rowNumber" : "colNumber" )}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public T Access(int rowNumber, int colNumber)
+        {
+            T val = default(T);
+            try
+            {
+                if (arr == null)
+                {
+                    throw new Exception("Array does not exist!!");
+                }
+                val = arr[rowNumber, colNumber];
+                Console.WriteLine($"value at rowNumber {rowNumber} colNumber {colNumber} is value {val}");
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+
+                Console.WriteLine($"Invalid Index used for {( rowNumber >= this.numberOfRows ? "rowNumber" : "colNumber" )}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return val;
+        }
+
+        public void Search(T value)
+        {
+            try
+            {
+                if (arr == null)
+                {
+                    throw new Exception("Array does not exist!!");
+                }
+                for (int i = 0; i < numberOfRows; i++)
+                {
+                    for (int j = 0; j < numberOfColumns; j++)
+                    {
+                        if (arr[i, j].Equals(value))
+                        {
+                            Console.WriteLine($"value {value} is present at rowNumber {i} colNumber {j} ");
+                            return;
+                        }
+                    }
+                }
+                Console.WriteLine($"value {value} is not present in array");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            }
+
+        public void Delete(int rowNumber, int colNumber)
+        {
+            try
+            {
+                if (arr == null)
+                {
+                    throw new Exception("Array does not exist!!");
+                }
+                arr[rowNumber, colNumber] = defaultValue;
+                Console.WriteLine($"value at rowNumber {rowNumber} colNumber {colNumber} set to defaultValue {defaultValue}");
+            }
+            catch (IndexOutOfRangeException)
+            {
+
+                Console.WriteLine($"Invalid Index used for {( rowNumber >= this.numberOfRows ? "rowNumber" : "colNumber" )}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void DeleteArray()
+        {
+            arr = null;
+        }
+    }
+}
--- DataStructuresAndAlgoByDSGuy/Array/Array/Program.cs
+++ DataStructuresAndAlgoByDSGuy/Array/Array/Program.cs
@@ -12,6 +12,24 @@ namespace Arrays
     class Giraffe : Animal
     {
     }
+
+    #region Array Info
+    /*
+     When to use:
+     1. When have large number of values of same type and number is known in advanced
+     2. Lot of random access operations     
+     When not to use:
+     1. When we have heterogenous values
+     2. When number of values is not known in advanced
+     Function           time complexity    space complexity
+     create array           o(1)               o(n) or o(mn)
+     insert                 o(1)               o(1)
+     traverse               o(n) or o(mn)      o(1)
+     search                 o(n) or o(mn)      o(1)
+     delete                 o(1)               o(1)
+     access                 o(1)               o(1)
+         */
+    #endregion
     class Program
     {
         static void Main(string[] args)
@@ -27,15 +45,38 @@ namespace Arrays
             //InsertForArray(arr, 2, 1);
             // InsertForArray(arr, 10, 10);
             //  Console.WriteLine(AccessCell(arr,1));
-            CustomArray<int> customArray = new CustomArray<int>(10, int.MinValue);
-            customArray.InsertAt(1, 90);
-            customArray.Traverse();
-            customArray.Delete(1);
-            customArray.Traverse();
-            customArray.InsertAt(9, 112);
-            customArray.AccessCell(9);
-            customArray.Search(112);
-            customArray.Search(11);
+            #region Custom array testing
+            //CustomArray<int> customArray = new CustomArray<int>(10, int.MinValue);
+            //customArray.InsertAt(1, 90);
+            //customArray.Traverse();
+            //customArray.Delete(1);
+            //customArray.Traverse();
+            //customArray.InsertAt(9, 112);
+            //customArray.AccessCell(9);
+            //customArray.Search(112);
+            //customArray.Search(11);
+            #endregion
+            #region Custom2DArray testing
+            Custom2DArray<int> custom2DArray = new Custom2DArray<int>(3, 6, int.MinValue);
+            custom2DArray.Traverse();
+            custom2DArray.Insert(0, 3,200) ;
+            custom2DArray.Traverse();
+            custom2DArray.Access(2, 3);
+            custom2DArray.Access(2, 7);
+            custom2DArray.Access(3, 3);
+            custom2DArray.Insert(0, 7, 100);
+            custom2DArray.Insert(3, 7, 100);
+            custom2DArray.Search(200);
+            custom2DArray.Search(300);
+            custom2DArray.Delete(0, 3);
+            custom2DArray.Traverse();
+            custom2DArray.DeleteArray();
+            custom2DArray.Traverse();
+            custom2DArray.Insert(2, 3, 22);
+            custom2DArray.Delete(2, 3);
+            custom2DArray.Search(22);
+            custom2DArray.Access(2, 3);
+            #endregion
         }
         static void DeleteItem<T>(T[] arr, int location)
         {
